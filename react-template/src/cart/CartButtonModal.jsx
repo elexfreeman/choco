ca05addc2e0f2a732476d09485e428a4756a2429
elex <elextraza@gmial.com>
2018-09-24 12:59:13 +0400

    корзина
    Сумма, итого цены

--- react-template/src/cart/CartButtonModal.jsx
+++ react-template/src/cart/CartButtonModal.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import CartModalItem from './CartModalItem'
+import {pricePipe} from "../base/pipes/price_pipe";
 
 
 // кнопка корзины в главном меню
@@ -10,11 +11,38 @@ class CartButtonModal extends Component {
     constructor(props) {
         super(props);
         this.onDelete = this.onDelete.bind(this);
+
+        let s = props.products.reduce((s, item) => s + (item.count * item.price), 0);
+        let cartLink = '/cart';
+        if ((localStorage.getItem('apiKey') !== null) && (localStorage.getItem('apiKey') !== '')) {
+            cartLink = '/user/cart';
+        }
+
+        this.state = {
+            summa: s
+            , cartLink: cartLink
+        }
     }
 
     onDelete(productId) {
         this.props.onDeletedCartItem(productId);
     }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate(prevProps) {
+        // Typical usage (don't forget to compare props):
+        if (this.props.products.length !== prevProps.products.length) {
+            /*высчитываем сумму*/
+            let s = this.props.products.reduce((s, item) => s + (item.count * item.price), 0);
+            this.setState({
+                summa: s
+            })
+        }
+    }
+
 
     render() {
         return (<div>
@@ -32,20 +60,25 @@ class CartButtonModal extends Component {
                                 <thead>
                                 <tr>
                                     <th></th>
-                                    <th>Товар</th>
+                                    <th style={{width: '30%'}}>Товар</th>
                                     <th>Кол-во</th>
+                                    <th>Цена</th>
+                                    <th>Сумма</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                {this.props.products.map((item, key) => <CartModalItem onDelete={this.onDelete}
-                                                                                       key={key} item={item}/>)}
+                                {this.props.products.map((item, key) =>
+                                    <CartModalItem onDelete={this.onDelete} key={key} item={item}/>
+                                )}
                                 </tbody>
                             </table>
+
+                            <div className='cart-modal-total'>Итого: <b>{pricePipe(this.state.summa)}</b> руб.</div>
 
                         </div>
                     </div>
                     <div className="modal-footer">
-                        <a href="/cart" className="btn">Перейти в корзину</a>
+                        <a href={this.state.cartLink} className="btn">Перейти в корзину</a>
                     </div>
                 </div>
             </div>
--- react-template/src/cart/CartModalItem.jsx
+++ react-template/src/cart/CartModalItem.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {rest_server} from "../models/settings";
+import {pricePipe} from "../base/pipes/price_pipe";
 
 const CartModalItem = ({item, onDelete}) => {
-  return (<tr>
-    <td><img className='product-img' src={item.main_img} /></td>
-    <td className='product-caption'>{item.caption}</td>
-    <td>{item.count} шт.</td>
+    return (<tr>
+        <td><img className='product-img'
+                 src={rest_server + 'img/w128/' + item.main_img.split('/')[item.main_img.split('/').length - 1]}/></td>
+        <td style={{width: '30%'}} className='product-caption'>{item.caption}</td>
+        <td>{item.count} шт.</td>
+        <td>{pricePipe(item.price)} руб.</td>
+        <td>{pricePipe(item.price * item.count)} руб.</td>
 
-  </tr>)
+    </tr>)
 
 }
 export default CartModalItem;
